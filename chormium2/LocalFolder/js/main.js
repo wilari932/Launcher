f732f8bbea1e86321211433c881ef1c9130d4257
wilmar <wilmar.arias@hotmail.com>
2020-05-01 18:31:51 +0200

    new
    saas

---
+++ chormium2/LocalFolder/js/main.js
@@ -0,0 +1,34 @@
+﻿
+
+
+var login = new Vue({
+    el: '#login',
+    data: {
+        username: window.localStorage.getItem('nausernameme'),
+        password: ''
+
+    },
+  
+    methods: {
+        ajax_login: function () {
+            var self = this;
+            $.ajax({
+                url: 'https://www.gamesow.com/jsp/login/',
+                data: { username: self.username, password: self.password, valid_code: true, game_id: 11, device: 'mini' },
+                dataType: "jsonp",
+                async: false,
+                success: function (data) {
+                    if (data.code === 1) {
+                        window.localStorage.setItem('nausernameme', self.username);
+                        location.href = "https://www.gamesow.com/playgame/11/";
+                       
+                       
+                        //location.href = 'https://www.gamesow.com/member/dologin?referer=https%3A%2F%2Fwww.gamesow.com%2Fplaygame%2F11%2F&l%3Des_es';
+                    } else {
+                        alert(data.msg);
+                    }
+                }
+            });
+        }
+    }
+});
